abc7ff8add5e1d115f7a99195df32e28fac6344c
migs2014 <migeledouglas@gmail.com>
2025-07-10 21:15:34 +0300

    Linking with cloudinary

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,46 +1,89 @@
 import User from "../model/userModel.js";
 import { errorHandleMiddleware } from "../middleware/errorHandleMiddleware.js";
 import ErrorHandler from "../middleware/errorMiddleware.js";
-import { jsontoken} from "../utils/token.js";
+import { jsontoken } from "../utils/token.js";
+import cloudinary from "cloudinary";
+export const createUserController = errorHandleMiddleware(
+  async (req, res, next) => {
+    try {
+      if (!req.files || Object.keys(req.files).length === 0) {
+        return next(new ErrorHandler("Image Required", 404));
+      }
+      const { avatar } = req.files;
+      const allowedFormats = ["image/png", "image/jpeg", "image/webp"];
+      if (!allowedFormats.includes(avatar.mimetype)) {
+        return next(new ErrorHandler("File Format Not supported", 400));
+      }
+      const {
+        name,
+        email,
+        password,
+        role,
+        phone,
+        address,
+        dateOfBirth,
+        gender,
+      } = req.body;
+      // Validator summarized
+      if (
+        !name ||
+        !email ||
+        !password ||
+        !role ||
+        !address ||
+        !phone ||
+        !dateOfBirth ||
+        !gender
+      ) {
+        return isNamedExportBindings(
+          new ErrorHandler("Fill all the fields", 400)
+        );
+      }
+      // Exiting user in database check here
+      const exitingUser = await User.findOne({ email });
+      if (exitingUser) {
+        return next(new ErrorHandler("This email Already Registered", 400));
+      }
+      // Cloudinary nad image code base
+      const cloudinaryResponse = await cloudinary.uploader.upload(
+        avatar.tempfilepath
+      );
+      if (!cloudinaryResponse || cloudinaryResponse.error) {
+        console.log(
+          "Cloudinary Error",
+          cloudinaryResponse.error || "unknown cloudinary error"
+        );
+        return next (new ErrorHandler("Failed to image to cloudinary",404))
+      }
+      const user = await User.create({
+        name,
+        email,
+        password,
+        role,
+        phone,
+        gender,
+        address,
+        dateOfBirth,
+        avatar: {
+          public_id: cloudinaryResponse.public_id,
+          url: cloudinaryResponse.secure_url
+        }
+      });
 
-export const createUserController = errorHandleMiddleware(async (req, res, next) => {
-  try {
-    const { name, email, password, role, phone, address, dateOfBirth, gender } =
-      req.body;
-    // Validator summarized
-    if (!name || !email || !password || !role || !address || !phone || !dateOfBirth || !gender){
-      return isNamedExportBindings(new ErrorHandler("Fill all the fields",400))
-    };
-   // Exiting user in database check here
-    const exitingUser = await User.findOne({ email });
-    if (exitingUser) {
-      return next (new ErrorHandler("This email Already Registered",400));
+      // response here
+      //  jsontoken (user, "User Created successfully",201, res);
+      res.status(200).json({
+        success: true,
+        message: "User created successfully",
+        data: user,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: "Failed to create User",
+        error,
+      });
     }
-
-    const user = await User.create({
-      name,
-      email,
-      password,
-      role,
-      phone,
-      gender,
-      address,
-      dateOfBirth,
-    }); 
-
-    // response here
-  //  jsontoken (user, "User Created successfully",201, res);
-  res.status(200).json({
-  success: true,
-  message: "User created successfully",
-  data: user
-});
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      message: "Failed to create User",
-      error,
-    });
   }
-});
+);
--- server/server.js
+++ server/server.js
@@ -6,11 +6,18 @@ import morgan from "morgan";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
 import { errorMiddleware } from "./middleware/errorMiddleware.js";
-
+import cloudinary from "cloudinary";
 const app = express();
-
+import fileUpload from "express-fileupload";
+import cookie from "cookie-parser";
+// cloudinary set up
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 //All port from dotenv
-const port = process.env.PORT||6060;
+const port = process.env.PORT || 6060;
 const url = process.env.MONGO_URL;
 
 //Middleware
@@ -21,16 +28,26 @@ app.use(
     credentials: true,
   })
 );
-app.use(express.json())
-app.use(morgan("dev"))
-
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookie());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    tempFileDir: "/tmp",
+    useTempFiles: true,
+  })
+);
 // mongodb connect
-mongoose.connect(url).then(()=>{
-    console.log(`Database connection successful`)
-}).catch((err)=>console.log("Database error is",err));
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log(`Database connection successful`);
+  })
+  .catch((err) => console.log("Database error is", err));
 
 // Routes are here
-app.use("/api/v1/user",userRoutes)
+app.use("/api/v1/user", userRoutes);
 
 // Server listen
 app.listen(port, () => {
